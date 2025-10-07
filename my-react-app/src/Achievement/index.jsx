b3547942b090793d03e6e9f4e9ff29848c878c1f
drpallid <drhowa4@ilstu.edu>
2025-10-06 19:26:42 -0500

    More editing of achievement layout

--- my-react-app/src/Achievement/index.jsx
+++ my-react-app/src/Achievement/index.jsx
@@ -6,18 +6,36 @@ export default function Achievement(props) {
  
     return (
         <>
-            <div className="Achievement">
+            <div className="AchievementSect">
                 <h2 className = "h2Achieve">Achievements</h2> {/*I'm having issues with how the stars below grow when hovered over, it screws with the margins, and I don't remember how to fx it/can't find it online either.*/}
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
-                <img className="sixths" src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                <div className="Achievements">
+                    <img src="src/assets/achievementStar.svg" alt="Rounded 5 star shape."></img>
+                    <h3>[achievement name]</h3>
+                </div>
+                
                 <p className="pAchieve">Challenge yourself to collect all of the achievements!</p>
             </div>
 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, eveniet. Sunt a nam error recusandae magni quas modi quidem. Suscipit laborum dolore placeat cumque repudiandae delectus sed id corporis. Accusamus nostrum placeat error iure officia laboriosam fugiat vero quisquam harum obcaecati quibusdam, modi ab fugit cupiditate esse quaerat. Consectetur, autem.</p>
         </>
         
     );

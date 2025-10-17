14bc8872001741d307c4df4af558ceb75579b99d
drpallid <drhowa4@ilstu.edu>
2025-10-17 08:08:10 -0500

    Trying bootstrap icons

--- my-react-app/src/InstructorProfile/ClassList/StudentList/index.jsx
+++ my-react-app/src/InstructorProfile/ClassList/StudentList/index.jsx
@@ -1,16 +1,37 @@
 import "./index.css";
 import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { CaretDownSquareFill, CaretRightSquareFill } from "react-bootstrap-icons";
+
 
 export default function Achievement(props) {
-
- 
+    function handleClick() {
+        console.log('Button clicked!');
+    }
     return (
         <>
             <div className="StudentLi">
                 {/*<button className='ClassSect'>Class 1</button>*/}
                     
-                    <div className='Student'><p className="StuPara"><a href="#">[X]</a> [student name]</p><button className="Remove">Remove</button></div>
+                    <div className='Student'>
+                        <p className="StuPara" onClick={handleClick}>
 
+                            
+
+                        [student name]</p>
+                        <button className="Remove">Remove</button>
+                    </div>
+
+
+
+                    <div className='Student'>
+                        <p className="StuPara">
+
+                            
+
+                        [student name]</p>
+                        <button className="Remove">Remove</button>
+                    </div>
 
             </div>
 

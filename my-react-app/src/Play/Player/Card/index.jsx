9d5882b33447119f0ee2d85deeb635b5a56ebce3
drpallid <drhowa4@ilstu.edu>
2025-10-29 20:55:54 -0500

    playing cards layout made- forgot to click add for these

--- my-react-app/src/Play/Player/Card/index.jsx
+++ my-react-app/src/Play/Player/Card/index.jsx
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 
 export default function Card(props) {
     return (
-        <img src={`/CardData/role/${props.image}`} alt="Placeholder card" className="CardWrapper" />   
+        <div className="playingCards">
+
+            <ul className="hand">
+                <li>
+                        <img className="card" src={`/CardData/role/${props.image}`} alt="Playing card" />
+                </li>
+            </ul>
+            <div className="clear"></div>
+
+        </div>
     );
 }
 

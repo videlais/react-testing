6efcd37b14bd284205e2a13000d194a9124119eb
Dan Cox <dan.cox@videlais.com>
2025-10-30 16:34:22 -0500

    Fixing complex state management for card selection

--- my-react-app/src/Play/Player/Card/index.jsx
+++ my-react-app/src/Play/Player/Card/index.jsx
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 
 export default function Card(props) {
     return (
-        <div className="playingCards">
-
-            <ul className="hand">
-                <li>
-                        <img className="card" src={`/CardData/role/${props.image}`} alt="Playing card" />
-                </li>
-            </ul>
-            <div className="clear"></div>
-
-        </div>
+        <li>
+            <label aria-label={`Select ${props.image}`}>
+                <input
+                    type="checkbox"
+                    checked={props.checked || false}
+                    onChange={props.onChange}
+                    value={props.image}
+                    className="card-checkbox"
+                    style={{
+                        backgroundImage: `url(/CardData/role/${props.image})`
+                    }}
+                />
+            </label>
+        </li>
     );
 }
 
 Card.propTypes = {
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    checked: PropTypes.bool,
+    onChange: PropTypes.func
 };
--- my-react-app/src/Play/Player/index.jsx
+++ my-react-app/src/Play/Player/index.jsx
@@ -1,16 +1,54 @@
 import "./index.css";
 import Card from './Card';
+import {useState} from 'react';
 import * as cards from '../../CardData/role/index.json';
 
 export default function Player(props) {
+
+// Define state for this component.
+// We will pass this to the child, Card.
+const [selectedCards, setSelectedCards] = useState([]);
+
+// As cards are "selected", we update the array.
+const handleCardChange = (cardImage, isChecked) => {
+    
+    if (isChecked) {
+        // Only allow selection if we haven't reached the limit of 3
+        if (selectedCards.length < 3) {
+            setSelectedCards([...selectedCards, cardImage]);
+        }
+        // If we're at the limit, the checkbox will remain unchecked (no state change)
+    } else {
+        // Always allow deselection regardless of current count
+        setSelectedCards(selectedCards.filter(card => card !== cardImage));
+    }
+};
+
   return(
       <div className="PlayerWrapper">
-
-          {cards.default.map((entry) => <Card 
-            key={entry.number} 
-            number={entry.number} 
-            image={entry.image} />)}
-            
+        <div className="playingCards">
+          <ul className="hand">
+            {
+              cards.default.map((entry) => 
+                <Card 
+                  key={entry.number} 
+                  number={entry.number} 
+                  image={entry.image}
+                  checked={selectedCards.includes(entry.image)}
+                  disabled={!selectedCards.includes(entry.image) && selectedCards.length >= 3}
+                  onChange={(e) => handleCardChange(entry.image, e.target.checked)} 
+                  />)
+            }
+          </ul>
+          <div className="clear"></div>
+          <div><strong>Selected</strong>
+            <ul>
+              {
+                selectedCards.map(entry => <li key={entry}>{entry.slice(0, -4)}</li>)
+              }
+            </ul>
+          </div>
+        </div>    
       </div>
   );
 }

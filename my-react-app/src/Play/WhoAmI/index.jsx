9300b92017f9e26d18ac7d3792c7a0b144b0775e
Dan Cox <dan.cox@videlais.com>
2025-11-11 12:05:18 -0600

    Added Who Am I

--- my-react-app/src/Play/WhoAmI/index.jsx
+++ my-react-app/src/Play/WhoAmI/index.jsx
@@ -1,19 +1,13 @@
 import "./index.css";
-import React from 'react';
 import PropTypes from 'prop-types';
 
 
 export default function WhoAmI({ folder, filename }) {
-  const imagePath = new URL(`../../../CardData/who-am-I/${folder}/${filename}`, import.meta.url).href;
-
   return (
-    
     <div
       className="WhoAmIWrapper"
-      style={{ backgroundImage: `url(${imagePath})` }}
-    >
-      <div className="CardLabel">Card {folder}</div>
-    </div>
+      style={{ backgroundImage: `url(/CardData/who-am-I/${folder}/${filename})` }}
+    />
   );
 }
 
